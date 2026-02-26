8bbcaf0e661f9aa3f701cf4c53911ba8f01ce5aa
Ry Suriyathep <rysu986@gmail.com>
2026-02-25 17:22:01 -0800

    report changes

--- backend/index.js
+++ backend/index.js
@@ -166,7 +166,10 @@ app.post("/contact", async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      family: 4, // 👈 Force IPv4
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
